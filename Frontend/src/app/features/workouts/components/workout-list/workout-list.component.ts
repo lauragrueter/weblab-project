24e9b1e32e439d7e9fe9118bbc4f57e8da6a8136
Laura <lauragrueter97@gmail.com>
2026-09-11 11:13:37 +0200

    💄 implement paginator for workouts list

--- Frontend/src/app/features/workouts/components/workout-list/workout-list.component.ts
+++ Frontend/src/app/features/workouts/components/workout-list/workout-list.component.ts
@@ -1,13 +1,14 @@
-import { Component, computed, inject, signal, OnInit } from '@angular/core';
+import { Component, computed, inject, signal, OnInit, viewChild, effect } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { MatTableModule } from '@angular/material/table';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { MatIconButton } from '@angular/material/button';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { WorkoutService } from '../../services/workout.service';
 import { WorkoutDialog } from '../workout-dialog/workout-dialog';
 import { Workout } from '../../models/workout.models';
@@ -23,14 +24,18 @@ import { Workout } from '../../models/workout.models';
     MatIconModule,
     MatButtonModule,
     MatDialogModule,
-    MatIconButton
+    MatIconButton,
+    MatPaginatorModule,
   ],
   templateUrl: './workout-list.component.html',
-  styleUrls: ['./workout-list.component.css']
+  styleUrls: ['./workout-list.component.css'],
 })
-export class WorkoutListComponent {
+export class WorkoutListComponent implements OnInit {
   private workoutService = inject(WorkoutService);
   private dialog = inject(MatDialog);
+
+  paginator = viewChild(MatPaginator);
+  dataSource = new MatTableDataSource<Workout>([]);
 
   displayedColumns: string[] = ['date', 'duration', 'name', 'actions'];
   searchTerm = signal('');
@@ -41,12 +46,18 @@ export class WorkoutListComponent {
 
     if (!term) return allWorkouts;
 
-    return allWorkouts.filter(w =>
-      w.name.toLowerCase().includes(term)
-    );
+    return allWorkouts.filter((w) => w.name.toLowerCase().includes(term));
   });
 
-  ngOnInit():void {
+  private syncPaginatorEffect = effect(() => {
+    this.dataSource.data = this.filteredWorkouts();
+    const paginatorRef = this.paginator();
+    if (paginatorRef) {
+      this.dataSource.paginator = paginatorRef;
+    }
+  });
+
+  ngOnInit(): void {
     this.workoutService.loadWorkouts();
   }
 
@@ -56,18 +67,18 @@ export class WorkoutListComponent {
   }
 
   editWorkout(workout: Workout): void {
-  const dialogRef = this.dialog.open(WorkoutDialog, {
-    width: '400px', //TODO keine fixe grösse
-    data: { workout }
-  });
+    const dialogRef = this.dialog.open(WorkoutDialog, {
+      width: '400px', //TODO keine fixe grösse
+      data: { workout },
+    });
 
-  dialogRef.afterClosed().subscribe((formData: Workout | undefined) => {
-    if (formData) {
-      const { id, ...dto } = formData;
-      this.workoutService.updateWorkout(workout.id, dto);
-    }
-  });
-}
+    dialogRef.afterClosed().subscribe((formData: Workout | undefined) => {
+      if (formData) {
+        const { id, ...dto } = formData;
+        this.workoutService.updateWorkout(workout.id, dto);
+      }
+    });
+  }
 
   deleteWorkout(id: string): void {
     this.workoutService.deleteWorkout(id);
