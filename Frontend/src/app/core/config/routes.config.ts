4e6152bb157d98a7e51191d83130e791b5e3da29
Laura <lauragrueter97@gmail.com>
2026-09-20 17:07:45 +0200

    ⚡️ improve performance and SEO score

--- Frontend/src/app/core/config/routes.config.ts
+++ Frontend/src/app/core/config/routes.config.ts
@@ -1,23 +1,20 @@
 import { Route } from '@angular/router';
 import { PATHS } from './path.config';
-import { Workouts } from '../../features/workouts/workouts';
-import { WorkoutCalendar } from '../../features/workouts/components/calendar/workout-calendar';
-import { Categories } from '../../features/categories/components/categories';
 
-const { WORKOUTS, WORKOUTCALENDAR,CATEGORIES } = PATHS;
+const { WORKOUTS, WORKOUTCALENDAR, CATEGORIES } = PATHS;
 
 export const routes: Route[] = [
   {
     path: WORKOUTS.path,
-    component: Workouts
+    loadComponent: () => import('../../features/workouts/workouts').then(m => m.Workouts)
   },
   {
     path: WORKOUTCALENDAR.path,
-    component: WorkoutCalendar
+    loadComponent: () => import('../../features/workouts/components/calendar/workout-calendar').then(m => m.WorkoutCalendar)
   },
   {
     path: CATEGORIES.path,
-    component: Categories
+    loadComponent: () => import('../../features/categories/components/categories').then(m => m.Categories)
   },
   {
     path: '**',
